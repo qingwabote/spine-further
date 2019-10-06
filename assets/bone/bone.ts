99f0ab1a3d2aaf87770206cb332eef5acda17fb2
liuzilong <qingwabote@126.com>
2019-10-06 22:33:20 +0800

    showcase for runtime bone position

---
+++ assets/bone/bone.ts
@@ -0,0 +1,23 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(sp.Skeleton)
+    skeleton: sp.Skeleton = null;
+
+    onLoad() {
+        let bone = this.skeleton.findBone('xian1');
+
+        this.node.on(cc.Node.EventType.TOUCH_MOVE, (e: cc.Event.EventTouch) => {
+            let local = this.skeleton.node.convertToNodeSpaceAR(
+                e.getLocation()
+            );
+
+            let boneWorld = cc.v2(local.x + bone.getWorldX(), local.y + bone.getWorldY());
+            let boneLocal = bone.worldToLocal(boneWorld);
+            bone.x = boneLocal.x
+            bone.y = boneLocal.y;
+        });
+    }
+}
